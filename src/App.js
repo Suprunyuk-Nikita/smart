c89851ab12055507b6b599d28bf3cee564dda368
Nikita_Suprunyuk <suprunyuk.nikita.gm@gmail.com>
2023-12-15 13:28:25 +0300

    Side panel, adaptation for PC and tablet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header.js';
+import Panel from './components/side-panel.js';
 
 function App() {
   return (
     <>
       <Header />
+      <div className='App-main'>
+        <Panel />
+        <div className='App-middle'>
+          .
+        </div>
+        <Panel />
+      </div>
     </>
   );
 }
---
+++ src/components/side-panel.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./side-panel.css";
+
+const Panel = () => {
+    return(
+        <div className="side-panel" />
+    );
+}
+
+export default Panel;
