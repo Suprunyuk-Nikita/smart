bfe726fbe341b7de7d95e7916e22b7efff5eb2c7
Nikita_Suprunyuk <suprunyuk.nikita.gm@gmail.com>
2023-12-15 23:36:41 +0300

    Footer, adaptation for PC and tablet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/header.js';
 import Panel from './components/side-panel.js';
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./footer.css";
+import wk from "../resources/images/vk-icon.png";
+import instagram from "../resources/images/instagram-icon.png";
+import facebook from "../resources/images/facebook-icon.png";
 
 const Footer = () => {
-    return(
+    return (
         <div className="footer">
-            .
+            <p className="footer-heading">Контактная информация:</p>
+            <p className="footer-text">Адрес: г. Кобрин, ул. Дружбы, 2 (здание «Эдельвейс»)</p>
+            <p className="footer-text">МТС: +375 (33) 911-18-81</p>
+            <p className="footer-text" style={{ marginBottom: "15px" }}>E-mail: smart_kids_adults@yahoo.com</p>
+            <p className="footer-heading" style={{ marginBottom: "10px" }}>Центр SMART в соцсетях:</p>
+            <div style={{ display: "block" }}>
+                <img className="footer-icon" src={wk} alt="#" />
+                    <img className="footer-icon" src={instagram} alt="#" />
+                <img className="footer-icon" src={facebook} alt="#" />
+            </div>
         </div>
     );
 }
